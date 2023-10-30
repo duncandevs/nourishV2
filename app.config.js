2243844529dc7a4a09d1bdf0d73f2f7f808a9a14
Duncan Maina <duncandevs@gmail.com>
2023-10-29 19:30:20 -0500

    add project id

--- app.config.js
+++ app.config.js
@@ -5,7 +5,10 @@ export default {
     extra: {
         SUPABASE_DB_URL: process.env.SUPABASE_DB_URL, 
         SUPABASE_DB_ANON_KEY: process.env.SUPABASE_DB_ANON_KEY, 
-        SUPABASE_DB_SERVICE_ROLE_KEY: process.env.SUPABASE_DB_SERVICE_ROLE_KEY
+        SUPABASE_DB_SERVICE_ROLE_KEY: process.env.SUPABASE_DB_SERVICE_ROLE_KEY,
+        eas: {
+            projectId: "f4669a33-c3ab-47c6-8988-4952ccb8587c"
+        }
     }
   },
 };
