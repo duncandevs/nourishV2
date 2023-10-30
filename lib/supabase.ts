6af37524fadd758abba8eda4ede6068431401e57
Duncan Maina <duncandevs@gmail.com>
2023-10-29 22:21:31 -0500

    remove ios build & update the env variables

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,16 +1,12 @@
 import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
-import Constants from 'expo-constants';
-console.log('Constants - ', Constants);
-// const SUPABASE_DB_URL = Constants.expoConfig?.extra?.SUPABASE_DB_URL;
-// const SUPABASE_DB_ANON_KEY = Constants.expoConfig?.extra?.SUPABASE_DB_ANON_KEY;
-// const SUPABASE_DB_SERVICE_ROLE_KEY = Constants.expoConfig?.extra?.SUPABASE_DB_SERVICE_ROLE_KEY;
+// import Constants from 'expo-constants';
+const SUPABASE_DB_SERVICE_ROLE_KEY = process.env.EXPO_PUBLIC_SUPABASE_SERVICE_KEY;
+const SUPABASE_DB_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+const SUPABASE_DB_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
 
-const SUPABASE_DB_URL="https://vxfwozbuuytymkppdusd.supabase.co"
-const SUPABASE_DB_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4ZndvemJ1dXl0eW1rcHBkdXNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYxOTA1NTksImV4cCI6MjAxMTc2NjU1OX0.AgzkBls8g_ohnKucg_XCluaz41zR2pniLk5IPGhPH9g"
-const SUPABASE_DB_SERVICE_ROLE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4ZndvemJ1dXl0eW1rcHBkdXNkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NjE5MDU1OSwiZXhwIjoyMDExNzY2NTU5fQ.cqBl57ztaUE8F7cZkRT37TzN4kdBHY7Sc9YWDxw-QVA"
-console.log('process env - ', process.env.EXPO_PUBLIC_TEST)
+
 // SUPABASE_DB_URL, SUPABASE_DB_ANON_KEY,
 export const supabase = createClient(SUPABASE_DB_URL, SUPABASE_DB_ANON_KEY , {
   auth: {
