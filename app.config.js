02338676e7b813697d0c4e969a7aeffd2c3972fe
Duncan Maina <duncandevs@gmail.com>
2023-10-29 22:39:41 -0500

    remove use on Constants and replace process.env with dotenv

--- app.config.js
+++ app.config.js
@@ -1,10 +1,11 @@
+// REQUIRED TO LOAD THE ENVIRONMENT VARIABLES IN .ENV
 import 'dotenv/config';
 
 export default {
   expo: {
     "name": "nourish",
     "slug": "nourish-fit",
-    "version": "1.0.7",
+    "version": "1.0.9",
     "orientation": "portrait",
     "icon": "./assets/nourish-app-icon.png",
     "userInterfaceStyle": "light",
@@ -34,9 +35,6 @@ export default {
     },
     "extra": {
       "eas": {
-        SUPABASE_DB_URL: process.env.SUPABASE_DB_URL, 
-        SUPABASE_DB_ANON_KEY: process.env.SUPABASE_DB_ANON_KEY, 
-        SUPABASE_DB_SERVICE_ROLE_KEY: process.env.SUPABASE_DB_SERVICE_ROLE_KEY,
         "projectId": "53eedea4-a767-42a9-95de-35fadd75f38d"
       }
     },
