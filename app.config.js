ca8ca5e501517a744d408d5cdf6cc3f48efa69ef
Duncan Maina <duncandevs@gmail.com>
2023-10-29 20:03:36 -0500

    move configs to app.config.js

--- app.config.js
+++ app.config.js
@@ -2,16 +2,50 @@ require('dotenv').config();
 
 export default {
   expo: {
-    extra: {
+    "name": "nourish",
+    "slug": "nourish-fit",
+    "version": "1.0.7",
+    "orientation": "portrait",
+    "icon": "./assets/nourish-app-icon.png",
+    "userInterfaceStyle": "light",
+    "splash": {
+      "image": "./assets/splash.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#ffffff"
+    },
+    "assetBundlePatterns": [
+      "**/*"
+    ],
+    "ios": {
+      "supportsTablet": true,
+      "bundleIdentifier": "com.nourish.fit",
+      "runtimeVersion": {
+        "policy": "nativeVersion",
+        "buildNumber": "2"
+      }
+    },
+    "android": {
+      "adaptiveIcon": {
+        "foregroundImage": "./assets/adaptive-icon.png",
+        "backgroundColor": "#ffffff"
+      },
+      "package": "com.nourish.fit",
+      "versionCode": 2
+    },
+    "web": {
+      "favicon": "./assets/favicon.png"
+    },
+    "extra": {
+      "eas": {
         SUPABASE_DB_URL: process.env.SUPABASE_DB_URL, 
         SUPABASE_DB_ANON_KEY: process.env.SUPABASE_DB_ANON_KEY, 
         SUPABASE_DB_SERVICE_ROLE_KEY: process.env.SUPABASE_DB_SERVICE_ROLE_KEY,
-        eas: {
-            projectId: "f4669a33-c3ab-47c6-8988-4952ccb8587c"
-        }
+        "projectId": "53eedea4-a767-42a9-95de-35fadd75f38d"
+      }
     },
-    ios: {
-        bundleIdentifier: "com.nourish.fit"
-    }
+    "owner": "theaifirstco",
+    "updates": {
+      "url": "https://u.expo.dev/53eedea4-a767-42a9-95de-35fadd75f38d"
+    },
   },
 };
