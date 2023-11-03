679792331b31219f87c46640a4f37057c780d0a9
Duncan Maina <duncandevs@gmail.com>
2023-11-03 18:26:54 -0500

    comment out alert

--- App.tsx
+++ App.tsx
@@ -30,7 +30,7 @@ const App = () => {
       }
     } catch (error) {
       // You can also add an alert() to see the error message in case of an error when fetching updates.
-      alert(`Error fetching latest Expo update: ${error}`);
+      // alert(`Error fetching latest Expo update: ${error}`);
     }
   };
 
